333affbbfb3693e2619aebb659c982c59be8c99e
yangbo <352420160@qq.com>
2019-08-07 00:25:32 +0800

    :tada:Initial commit

---
+++ src/main/java/com/netty/firstexample/TestServer.java
@@ -0,0 +1,41 @@
+package com.netty.firstexample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * 服务器
+ *
+ * @author YangBo
+ * @date 2019/08/06
+ */
+public class TestServer {
+    public static void main(String[] args) {
+        // 线程组：接收
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+
+        // 线程组：处理
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+
+            // 服务端启动
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).childHandler(new TestServerInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+
+}
---
+++ src/main/java/com/netty/firstexample/TestServerInitializer.java
@@ -0,0 +1,24 @@
+package com.netty.firstexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ * 服务端初始化器
+ *
+ * @author YangBo
+ * @date 2019/08/06
+ */
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast("httpServerCodec",new HttpServerCodec());
+        pipeline.addLast("testHttpServerHandler", new TestHttpServerHandler());
+    }
+}
